225caa096cfa072ce4fc8fc145ee53e6e774f45d
Igor Skiljevic <igor.skiljevic@saga.rs>
2020-05-19 16:06:21 +0200

    importi

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ Setup
 
 ```
 git clone https://github.com/IgorSkiljevic/tennis
-cd elm-ts-todomvc
 npm i
 npm start
 open index.html
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { cmd } from 'elm-ts/lib'
+import { Cmd } from 'elm-ts/lib/Cmd'
+import { Html } from 'elm-ts/lib/React'
+import { perform } from 'elm-ts/lib/Task'
+import { Task } from 'fp-ts/lib/Task'
+import * as React from 'react'
